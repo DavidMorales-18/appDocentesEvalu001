1b3000fbd11f631a3105411d71c543e22d7b27cf
DavidMorales-18 <bmoralest84@gmail.com>
2022-12-11 17:15:20 -0500

    scroll registro

--- src/Registration.js
+++ src/Registration.js
@@ -1,119 +1,122 @@
-import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
-import React, { useState } from 'react';
-import { firebase } from '../config';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+import React, { useState } from "react";
+import { firebase } from "../config";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 /* import { useNavigation } from '@react-navigation/native'; */
 
 const Registration = () => {
   /* const navigation = useNavigation() */
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [lastname, setLastname] = useState('');
-  const [firstname, setFirstname] = useState('');
-  const [seudonim, setSeudonim] = useState('');
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [seudonim, setSeudonim] = useState("");
 
   RegisterUser = async (email, password, lastname, firstname, seudonim) => {
-
-    await firebase.auth().createUserWithEmailAndPassword(email, password)
+    await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
       .then(() => {
-
-        firebase.auth().currentUser.sendEmailVerification({
-          handleCodeInApp: true,
-          url: 'https://docente-react-firebase.firebaseapp.com',
-        })
-          .then(() => {
-            alert('Verifica el Mail')
-          }).catch((error) => {
-            alert(error.message)
+        firebase
+          .auth()
+          .currentUser.sendEmailVerification({
+            handleCodeInApp: true,
+            url: "https://docente-react-firebase.firebaseapp.com",
           })
           .then(() => {
-            firebase.firestore().collection('users')
+            alert("Verifica el Mail");
+          })
+          .catch((error) => {
+            alert(error.message);
+          })
+          .then(() => {
+            firebase
+              .firestore()
+              .collection("users")
               .doc(firebase.auth().currentUser.uid)
               .set({
                 firstname,
                 lastname,
                 email,
-                seudonim
-              })
+                seudonim,
+              });
           })
-          .catch((error => {
-            alert(error.message)
-          }))
-
-      })
-
-
-  }
+          .catch((error) => {
+            alert(error.message);
+          });
+      });
+  };
 
   return (
-    <View style={style.container}>
-      <Text style={{ fontWeight: 'bold', fontSize: 23 }}>
-        Registrate
-      </Text>
-      <View style={{ marginTop: 40 }} >
-        <TextInput
-          style={style.textInput}
-          placeholder='Nombre'
-          onChangeText={(firstname) => setFirstname(firstname)}
-          autoCapitalize='nome'
-          autoCorrect={false}
-        />
-        <TextInput
-          style={style.textInput}
-          placeholder='Apellidos'
-          onChangeText={(lastname) => setLastname(lastname)}
-          autoCapitalize='nome'
-          autoCorrect={false}
-        />
-        <TextInput
-          style={style.textInput}
-          placeholder='Seudonim'
-          onChangeText={(seudonim) => setSeudonim(seudonim)}
-          autoCapitalize='nome'
-          autoCorrect={false}
-        />
+    <KeyboardAwareScrollView>
+      <View style={style.container}>
+        <Text style={{ fontWeight: "bold", fontSize: 23 }}>Registrate</Text>
+        <View style={{ marginTop: 40 }}>
+          <TextInput
+            style={style.textInput}
+            placeholder="Nombre"
+            onChangeText={(firstname) => setFirstname(firstname)}
+            autoCapitalize="nome"
+            autoCorrect={false}
+          />
+          <TextInput
+            style={style.textInput}
+            placeholder="Apellidos"
+            onChangeText={(lastname) => setLastname(lastname)}
+            autoCapitalize="nome"
+            autoCorrect={false}
+          />
+          <TextInput
+            style={style.textInput}
+            placeholder="Seudonim"
+            onChangeText={(seudonim) => setSeudonim(seudonim)}
+            autoCapitalize="nome"
+            autoCorrect={false}
+          />
 
-        <TextInput
-          style={style.textInput}
-          placeholder='email'
-          onChangeText={(email) => setEmail(email)}
-          autoCapitalize='nome'
-          autoCorrect={false}
-        />
-        <TextInput
-          style={style.textInput}
-          placeholder='Password'
-          onChangeText={(password) => setPassword(password)}
-          autoCapitalize='nome'
-          autoCorrect={false}
-          secureTextEntry={true}
-        />
-
+          <TextInput
+            style={style.textInput}
+            placeholder="email"
+            onChangeText={(email) => setEmail(email)}
+            autoCapitalize="nome"
+            autoCorrect={false}
+          />
+          <TextInput
+            style={style.textInput}
+            placeholder="Password"
+            onChangeText={(password) => setPassword(password)}
+            autoCapitalize="nome"
+            autoCorrect={false}
+            secureTextEntry={true}
+          />
+        </View>
+        <TouchableOpacity
+          onPress={() =>
+            RegisterUser(email, password, lastname, firstname, seudonim)
+          }
+          style={style.button}
+        >
+          <Text style={{ fontWeight: "bold", fontSize: 22 }}>
+            Registrate!!!
+          </Text>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity
-        onPress={() => RegisterUser(email, password, lastname, firstname, seudonim)}
-        style={style.button}
-      >
-        <Text style={{ fontWeight: 'bold', fontSize: 22 }}>
-          Registrate!!!
-        </Text>
+    </KeyboardAwareScrollView>
+  );
+};
 
-      </TouchableOpacity>
-
-    </View>
-  )
-
-
-}
-
-export default Registration
-
-
+export default Registration;
 
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10,
   },
   textInput: {
@@ -122,17 +125,17 @@ const style = StyleSheet.create({
     width: 400,
     fontSize: 20,
     borderBottomWidth: 1,
-    borderBottomColor: '#000',
+    borderBottomColor: "#000",
     marginBottom: 10,
-    textAlign: 'center'
+    textAlign: "center",
   },
   button: {
     marginTop: 50,
     height: 70,
     width: 250,
-    backgroundColor: '#026efd',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 50
-  }
-})
+    backgroundColor: "#026efd",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+  },
+});
